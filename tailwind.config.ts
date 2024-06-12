981e8e61a0cc8935db5590ce2fd8268998e9d18d
ramazanix <ramazanui2002@gmail.com>
2024-06-13 01:59:13 +0300

    Extended theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: "rgb(var(--background-color))",
+        first: "rgb(var(--first-color))",
+        second: "rgb(var(--second-color))",
+        third: "rgb(var(--third-color))",
+        fourth: "rgb(var(--fourth-color))",
+        fifth: "rgb(var(--fifth-color))",
       },
     },
   },
